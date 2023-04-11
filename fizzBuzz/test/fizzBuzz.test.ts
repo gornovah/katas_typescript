be7ac9fe626ebd43e80fc30da79c4f56d6734a25
Domingo Espinosa Medina <domingo.em@gmail.com>
2023-04-11 16:14:23 +0200

    refactor to add new test of multiple of 3

--- fizzBuzz/test/fizzBuzz.test.ts
+++ fizzBuzz/test/fizzBuzz.test.ts
@@ -3,15 +3,23 @@ import { fizzBuzz } from "../src/fizzBuzz";
 describe("fizzBuzz", () => {
   it.each`
     input | output
-    ${1}  | ${"1"}
-    ${2}  | ${"2"}
     ${3}  | ${"Fizz"}
-    ${4}  | ${"4"}
-    ${5}  | ${"Buzz"}
     ${6}  | ${"Fizz"}
     ${9}  | ${"Fizz"}
-    ${10} | ${"Buzz"}
     ${12} | ${"Fizz"}
+  `(
+    "return the multiple of 3-> $input to be the string $output",
+    ({ input, output }) => {
+      expect(fizzBuzz(input)).toEqual(output);
+    }
+  );
+  it.each`
+    input | output
+    ${1}  | ${"1"}
+    ${2}  | ${"2"}
+    ${4}  | ${"4"}
+    ${5}  | ${"Buzz"}
+    ${10} | ${"Buzz"}
     ${15} | ${"BuzzFizz"}
     ${30} | ${"BuzzFizz"}
   `(
