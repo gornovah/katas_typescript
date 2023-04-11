995917f04213587a1cc3467ced18f034ce87c084
Domingo Espinosa Medina <domingo.em@gmail.com>
2023-04-11 15:56:05 +0200

    remove comments

--- fizzBuzz/test/fizzBuzz.test.ts
+++ fizzBuzz/test/fizzBuzz.test.ts
@@ -1,14 +1,5 @@
 import { fizzBuzz } from "../src/fizzBuzz";
 
-// describe.each([
-//   { a: 1, b: 1, expected: 2 },
-//   { a: 1, b: 2, expected: 3 },
-//   { a: 2, b: 1, expected: 3 }
-// ])('.add($a, $b)', ({a, b, expected}) => {
-//   test(`returns ${expected}`, () => {
-//     expect(a + b).toBe(expected);
-//   });
-
 describe("fizzBuzz", () => {
   it.each`
     input | output
